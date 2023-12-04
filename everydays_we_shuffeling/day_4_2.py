16de3fb8ea3312097f02c404403b9a683d0bd6ec
Sibo523 <121120413+Sibo523@users.noreply.github.com>
2023-12-04 22:51:02 +0200

    Update day_4_2.py

--- everydays_we_shuffeling/day_4_2.py
+++ everydays_we_shuffeling/day_4_2.py
@@ -9,23 +9,19 @@ def main():
 
         for l_num,line in enumerate(D):
             array[l_num] += 1
-            i = 0
             sp = line[9:].split('|')
-            # print(sp)
             man = set()
             woman = set()
             for n in re.finditer(r'\d+', sp[0]):
                 man.add(n.group())
             for n in re.finditer(r'\d+', sp[1]):
                 woman.add(n.group())
-
-            for o in woman&man:
-                # print(o , end = " ")
-                i+=1        # amounts of wins
+            i = len(woman & man) #amount of overlaps -> wins
 
             for j in range (i): #runs the amoung of wins
                 array[l_num+1+j] +=array[l_num] #all the next index's should be summed with the current value in the box
 
         print(sum(array.values()))# sum all the values in the end :)
+        
 if __name__ == '__main__':
     print(f'time took: {timeit.timeit(main, number=1)}')
