53c8fce0a52037bd33d879af2cbe86df382a57ca
Sibo523 <121120413+Sibo523@users.noreply.github.com>
2023-12-06 16:58:29 +0200

    Update day_6.py
    simply better

--- everydays_we_shuffeling/day6/day_6.py
+++ everydays_we_shuffeling/day6/day_6.py
@@ -1,91 +1,15 @@
 
-import numpy
-import math
+import numpy,re,math
+
 def winning_races(total_time, record_distance):
     root1 = ((total_time + ((total_time**2 -4*record_distance)**(1/2)))/2 - 0.05)
     root2 = ((total_time - ((total_time**2 -4*record_distance)**(1/2)))/2 + 0.05)
     return math.floor(root1)-math.ceil(root2) + 1
 def main(file):
-    time = [int(i) for i in file[0].split() if i.isdigit()]
-    distance = [int(i) for i in file[1].split() if i.isdigit()]
-    list = []
-    for i in range(len(time)):
-        min_s = int(distance[i]/time[i])
-        list.append(winning_races(int(time[i]),int(distance[i])))
-    print(numpy.prod(list))
+    times =''.join( re.findall(r'\d+', file[0]))
+    distance = ''.join(re.findall(r'\d+', file[1]))
+    print(winning_races(int(times),int(distance)))
 
 if __name__ == '__main__':
     with open('day_6.txt') as file:
         main(file.readlines())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
