ea648244ef3cbf31e248305b3cb02dc4f507c50e
Sibo523 <121120413+Sibo523@users.noreply.github.com>
2023-12-06 21:34:20 +0200

    Update day_5_someone.py
    this is really close to what I did, but this works and looks much better than mine

--- everydays_we_shuffeling/day5/day_5_someone.py
+++ everydays_we_shuffeling/day5/day_5_someone.py
@@ -1,60 +1,31 @@
-from functools import reduce
-import timeit
+inputs, *blocks = open(0).read().split("\n\n")
 
-# Read input data from 'day_5.txt'
-seeds, *mappings = open('day_5.txt').read().split('\n\n')
+inputs = list(map(int, inputs.split(":")[1].split()))
 
-seeds = list(map(int, seeds.split()[1:]))
+seeds = []
 
-def lookup(inputs, mapping):
-    """
-    A generator function that performs memory lookups based on given inputs and memory mapping.
+for i in range(0, len(inputs), 2):
+    seeds.append((inputs[i], inputs[i] + inputs[i + 1]))
 
-    Parameters:
-    - inputs (list): List of tuples containing start positions and lengths to look up.
-    - mapping (str): String representation of memory mapping rules.
-
-    Yields:
-    Tuple: Represents the result of the memory lookup (start position, length).
-    """
-    for start, length in inputs:
-        print(f'{start = }')
-        print(f'{length = }')
-        while length > 0:
-            for m in mapping.split('\n')[1:]:
-                # Extract destination, source, and length from the mapping rule
-
-                dst, src, len = map(int, m.split())
-                # Calculate the difference between the start position and source position
-                delta = start - src
-                if delta in range(len):
-                    # Adjust the length based on the delta, yielding the result
-                    len = min(len - delta, length)
-                    yield (dst + delta, len)
-                    start += len
-                    length -= len
-                    break
-            else:
-                # If no match is found in the mapping, yield the original input tuple
-                yield (start, length)
+for block in blocks:
+    ranges = []
+    for line in block.splitlines()[1:]:
+        ranges.append(list(map(int, line.split())))
+    new = []
+    while len(seeds) > 0:
+        s, e = seeds.pop()
+        for a, b, c in ranges:
+            os = max(s, b)
+            oe = min(e, b + c)
+            if os < oe:
+                new.append((os - b + a, oe - b + a))
+                if os > s:
+                    seeds.append((s, os))
+                if e > oe:
+                    seeds.append((oe, e))
                 break
+        else:
+            new.append((s, e))
+    seeds = new
 
-def main():
-    """
-    The main function that processes the input seeds and mappings to find the minimum memory position.
-
-    Prints:
-    The minimum memory positions for two sets of inputs:
-    1. All seeds with a length of 1.
-    2. Pairs of seeds where the first seed is used as the start position, and the second seed is the length.
-    """
-    # Perform memory lookups for two sets of inputs and print the minimum memory positions
-    results = [min(reduce(lookup, mappings, s))[0] for s in [
-        zip(seeds, [1] * len(seeds)),   # All seeds with a length of 1
-        zip(seeds[0::2], seeds[1::2])   # Pairs of seeds as (start, length)
-    ]]
-    print(*results)
-
-if __name__ == '__main__':
-    # Measure the time taken to execute the main function and print the result
-    print(f'Time took: {timeit.timeit(main, number=1)}')
+print(min(seeds)[0])
