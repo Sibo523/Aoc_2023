d81f3b539952b32b418b27119733e871cfc67832
Sibo523 <121120413+Sibo523@users.noreply.github.com>
2023-12-08 21:12:53 +0200

    Update day_8_2.py

--- everydays_we_shuffeling/day8/day_8_2.py
+++ everydays_we_shuffeling/day8/day_8_2.py
@@ -1,4 +1,3 @@
-
 from math import gcd
 
 def main(file):  # remember to split ', ' for the keys to the next thing
@@ -9,17 +8,15 @@ def main(file):  # remember to split ', ' for the keys to the next thing
     #O(n)
     list = [i for i in Dict.keys() if i[-1] == 'A']
 
-    real_counter = 0
     prod = 1
     for cur in list:
         real_counter = 0
         while not cur[-1] == 'Z' :
-            ch = instruction[counter]
-            left, right = Dict[cur].split(', ')
-            cur = left if ch == 'L' else right
-            instruction = instruction[1:] + instruction[0] #used the elegent way described in part 1
+            left, right = Dict[cur].split(', ')             #have the split roat infront of you!
+            cur = left if instruction[0] == 'L' else right  #choose the path you are going
+            instruction = instruction[1:] + instruction[0]
             real_counter += 1
-        prod =prod * real_counter// gcd(prod,real_counter)
+        prod =prod * real_counter // gcd(prod,real_counter) #real_counter won't be 0 source trust me bro
     print(prod)
 
 
