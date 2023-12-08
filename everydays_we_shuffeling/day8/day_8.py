314835af36daf16186c18be327337752a1f57010
Sibo523 <121120413+Sibo523@users.noreply.github.com>
2023-12-08 20:48:20 +0200

    Update day_8.py

--- everydays_we_shuffeling/day8/day_8.py
+++ everydays_we_shuffeling/day8/day_8.py
@@ -17,6 +17,11 @@ def main(file): # remember to split ', ' for the keys to the next thing
         cur = left if ch == 'L' else right
         counter = (counter + 1) % len(instruction)
         real_counter += 1
+        """
+        something I didn't think of is just substringing the instruction like this
+        instruction = intruction[1:] +instruction[0]
+        this way we queue the first instruct to the end and move on to the next instruct in the 'queue'
+        """
     print(real_counter)
 
 if __name__ == '__main__':
