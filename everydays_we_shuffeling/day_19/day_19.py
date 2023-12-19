4325818e0035e836ab55dcb2ac0bb32c1ecc2de6
Sibo523 <roi.siboni@msmail.ariel.ac.il>
2023-12-19 20:57:48 +0200

    good day

--- everydays_we_shuffeling/day_19/day_19.py
+++ everydays_we_shuffeling/day_19/day_19.py
@@ -1,3 +1,27 @@
-mapy,instruction = open('day19.txt').read().split('\n\n') #differ the map to the instruction
-#^([^\w])*([\w\-]+)
-dic = {}
+mapy, instruction = open('day19.txt').read().split('\n\n')  # Split the map and instruction
+dic = {key: line[:-1].split(',') for key, line in (i.split('{') for i in mapy.split('\n'))}
+socially_accepted = []
+
+def x_max(x, m, a, s):
+    global dic
+    cur = 'in'
+    while True:
+        if cur in 'RA':
+            return cur == 'A'
+        for i in dic[cur]:
+            if ':' in i: #if I found
+                val, to = i.split(':')
+                if eval(val): #if it's true then I want to go where it tells me too so I break from the current instruction
+                    cur = to
+                    break
+            elif i in 'RA':
+                return i == 'A'
+            else:
+                cur = i
+
+
+
+for instru in instruction.split('\n'):
+    x, m, a, s = [int(variable.split('=')[-1]) for variable in instru[1:-1].split(',')]
+    socially_accepted.append(x + m + a + s) if (x_max(x, m, a, s)) else None
+print(sum(socially_accepted))
